09f1c42c3a785a69fad37265f4fc52b6456b74ba
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-11 10:02:46 +0300

    Spring

--- src/main/java/com/hamitmizrak/thy_design_pattern/ThyDesignPatternApplication.java
+++ src/main/java/com/hamitmizrak/thy_design_pattern/ThyDesignPatternApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ThyDesignPatternApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ThyDesignPatternApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ThyDesignPatternApplication.class, args);
+    }
 
 }
