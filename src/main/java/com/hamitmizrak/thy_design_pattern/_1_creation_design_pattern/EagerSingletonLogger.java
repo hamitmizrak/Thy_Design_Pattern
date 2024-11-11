6289412ce458f78a87828ceae38c36349007bf0f
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-11 11:53:40 +0300

    Eager Logger Singleton Design Pattern

---
+++ src/main/java/com/hamitmizrak/thy_design_pattern/_1_creation_design_pattern/EagerSingletonLogger.java
@@ -0,0 +1,22 @@
+package com.hamitmizrak.thy_design_pattern._1_creation_design_pattern;
+
+// Log işlemlerinde bir uygulamanın merkezi bir log yönetimine ihtiyaç duyarsak
+// Uygulamada aynı log yöneim nesnesini kullanmak istiyorsak
+public class EagerSingletonLogger {
+
+    // Instance
+    private static final EagerSingletonLogger instance= new EagerSingletonLogger();
+
+    // Private Constructor
+    private EagerSingletonLogger() {}
+
+    // Eğer nesne daha önceden oluşturulmamışsa aynı nesneyi kullan yoksa eskisini kullana
+    public static EagerSingletonLogger getInstance(){
+        return instance;
+    }
+
+    // Mesajı dönder
+    public void log(String message){
+        System.out.println("Log: "+message);
+    }
+}
