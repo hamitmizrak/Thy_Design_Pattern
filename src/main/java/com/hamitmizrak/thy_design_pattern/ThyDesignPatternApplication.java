89b1ad48bf47d8ee54856fb99d47e32013d9fa07
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-11 09:56:17 +0300

    Initial commit

---
+++ src/main/java/com/hamitmizrak/thy_design_pattern/ThyDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.thy_design_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThyDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThyDesignPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamitmizrak/thy_design_pattern/ThyDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.thy_design_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThyDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
