3a799e9e2ad9620cbc729802161925674cad95e9
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-11 11:02:22 +0300

    pom.xml Lombok, Mysql

---
+++ src/main/java/com/hamitmizrak/thy_design_pattern/_1_creation_design_pattern/DatabaseConnection.java
@@ -0,0 +1,8 @@
+package com.hamitmizrak.thy_design_pattern._1_creation_design_pattern;
+
+// Veri tabanı bağlantısını sistem  kaynakları çok yoğun bir şekilde kullanıdğı için
+// her seferinde yeni bir bağlantı instance(new) oluşturmadan tek bir bağlantı olmasını sağlayacağız.
+public class DatabaseConnection {
+
+    private static
+}
