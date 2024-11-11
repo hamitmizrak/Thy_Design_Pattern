790a0bf11e50d2ca90db1f81374720969f43a4a9
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-11 11:51:01 +0300

    Lazy Logger Singleton Design Pattern

---
+++ src/main/java/com/hamitmizrak/thy_design_pattern/_1_creation_design_pattern/LazySingletonLogger.java
@@ -0,0 +1,25 @@
+package com.hamitmizrak.thy_design_pattern._1_creation_design_pattern;
+
+// Log işlemlerinde bir uygulamanın merkezi bir log yönetimine ihtiyaç duyarsak
+// Uygulamada aynı log yöneim nesnesini kullanmak istiyorsak
+public class LazySingletonLogger {
+
+    // Instance
+    private static LazySingletonLogger instance;
+
+    // Private Constructor
+    private LazySingletonLogger() {}
+
+    // Eğer nesne daha önceden oluşturulmamışsa aynı nesneyi kullan yoksa eskisini kullana
+    public static LazySingletonLogger getInstance(){
+        if(instance == null){
+            instance = new LazySingletonLogger();
+        }
+        return instance;
+    }
+
+    // Mesajı dönder
+    public void log(String message){
+        System.out.println("Log: "+message);
+    }
+}
